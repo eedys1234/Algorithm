0a5bb5289482b0bb78e0a22fb2d69ee34e31554a
이정환 <eedys1234@naver.com>
2020-12-26 23:45:36 +0900

    recursion

---
+++ Recursion/src/Perm.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Perm {
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			String line = sc.nextLine();
+			String[] arg = line.split("\\s");
+			int n = Integer.valueOf(arg[0]);
+			int m = Integer.valueOf(arg[1]);
+			
+			int[] iset = new int[n];
+			
+			for(int i=1;i<=n;i++) {
+				iset[i-1] = i;
+			}
+			
+			perm(iset, m, 0);
+			
+		}
+	}
+	
+	public static void perm(int[] iset, int m, int k) {
+
+		if(k == m) {
+			for(int i=0;i<m;i++) {
+				System.out.print(iset[i] + " ");				
+			}
+			System.out.println();
+		}
+		
+		for(int i=k;i<iset.length;i++) {
+			swap(iset, i, k);
+			perm(iset, m, k+1);
+			swap(iset, i, k);
+		}
+	}
+	
+	public static void swap(int[] iset, int i, int k) {
+		int temp = iset[i];
+		iset[i] = iset[k];
+		iset[k] = temp;
+	}
+}
---
+++ Recursion/src/TaillCalls.java
@@ -0,0 +1,28 @@
+
+public class TaillCalls <T> {
+
+	public static <T> TailCall<T> call(TailCall<T> nextCalls) {
+		return nextCalls;
+	}
+	
+	public static <T> TailCall<T> done(T value) {
+		return new TailCall<T>() {
+
+			@Override
+			public TailCall<T> apply() {
+				throw new IllegalArgumentException();
+			}
+			
+			@Override
+			public boolean isComplete() {
+				return true;
+			}
+			
+			@Override
+			public T result() {
+				return value;				
+			}
+			
+		};
+	}
+}
