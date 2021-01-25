b670874196873a2d325ba5bcc75edc5c26576ca8
이정환 <eedys1234@naver.com>
2021-01-25 16:29:18 +0900

    back tracking

--- Recursion/src/BackJun/P15652.java
+++ Recursion/src/BackJun/P15652.java
@@ -3,23 +3,24 @@ package BackJun;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class P15649 {
+public class P15652 {
 
 	public static int[] value;
 	public static boolean[] visited;
 	public static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) {
-		
+
 		BufferedReader inbr = new BufferedReader(new InputStreamReader(System.in));
 		
 		try {
-			
-			String line = inbr.readLine();
+			String line = inbr.readLine();			
 			String[] temp = line.split(" ");
+			
 			
 			int n = Integer.valueOf(temp[0]);
 			int m = Integer.valueOf(temp[1]);
+			
 			value = new int[n];
 			visited = new boolean[n];
 			int[] space = new int[n];
@@ -27,36 +28,30 @@ public class P15649 {
 			for(int i=0;i<value.length;i++) {
 				value[i] = i+1;
 			}
-			
-			DFS(space, m, 0);
 
+			DFS(space, m, 0, 0);
 			System.out.println(sb.toString());
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	
-	public static void DFS(int[] space, int m, int depth) {
+	public static void DFS(int[] space, int m, int k, int depth) {
 		
-		if(depth == m) {
+		if(m == depth) {
 			for(int i=0;i<m;i++) {
 				sb.append(space[i]);
-				sb.append(" ");				
-			}			
+				sb.append(" ");
+			}
+			
 			sb.append("\n");
 		}
 		else {
-			for(int i=0;i<value.length;i++) {
-				if(!visited[i]) {
-					space[depth] = value[i];				
-					visited[i] = true;
-					DFS(space, m, depth+1);
-					visited[i] = false;
-				}
-			}			
-		}		
-		
+			
+			for(int i=k;i<value.length;i++) {
+				space[depth] = value[i];
+				DFS(space, m, i, depth+1);
+			}
+		}
 	}
-	
 }
