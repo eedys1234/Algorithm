853e8a26e211c979c6b63d4307301a02e2ed33e6
이정환 <eedys1234@naver.com>
2021-02-01 06:45:34 +0900

    lis

--- Recursion/src/BackJun/P14003.java
+++ Recursion/src/BackJun/P14003.java
@@ -2,23 +2,23 @@ package BackJun;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class P14003 {
 
 	public static void main(String[] args) {
 		
 		BufferedReader inbr = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> stack = new Stack<Integer>();
 
 		try {
 			
 			int n = Integer.valueOf(inbr.readLine());
 			int[] sequence = new int[n+1];
 			int[] lis = new int[n+1];
-			String[] path = new String[n+1];
-			StringBuilder sb = new StringBuilder();
+			int[] path = new int[n+1];
 			
 			int count = 0;
-			int max = 0;
 			int l=0;
 			String[] temp = inbr.readLine().split(" ");
 			
@@ -29,8 +29,9 @@ public class P14003 {
 			int j = 0;
 			if(n>=1) {
 				lis[1] = sequence[1];
-				path[1] = sequence[1] + " "; 
+				path[1] = 1;
 				j = 1;				
+				l = 1;
 			}
 			
 			for(int i=2;i<sequence.length;i++) {
@@ -38,34 +39,34 @@ public class P14003 {
 				if(lis[j] < sequence[i]) {
 					j+=1;
 					lis[j] = sequence[i];
-					
-					sb.append(path[j]);
-					sb.append(sequence[i]);
-					sb.append(" ");
+					path[i] = j;
 				}
 				else {
 					int idx = binarySearch(lis, 1, j, sequence[i]);
 					lis[idx] = sequence[i];
-					
-					for(int k=1;k<=idx;k++) {
-						sb.append(path[i]);
-						sb.append(lis[k]);
-						sb.append(" ");
-					}
-				}
-				path[i] = sb.toString();
-				sb.delete(0, sb.length());
-				
-				if(max < path[i].split(" ").length) {
-					l = i;
-				}
 
+					path[i] = idx;
+				}
 			}
 			
 			count = j;
 			System.out.println(count);
-			System.out.println(path[l].substring(0, path[l].length()-1));
+						
+			for(int i=path.length-1;i>=1;i--) {
+				if(count==path[i]) {
+					stack.push(sequence[i]);
+					count--;
+				}
+			}
+			
+			StringBuilder sb = new StringBuilder();
+			
+			while(!stack.isEmpty()) {
+				sb.append(stack.pop());
+				sb.append(" ");
+			}
 
+			System.out.print(sb.toString().substring(0, sb.toString().length()-1));
 
 		} catch(Exception e) {
 			e.printStackTrace();
