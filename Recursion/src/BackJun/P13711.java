b303743bc16c875476710eab63fadbd105af2ce0
이정환 <eedys1234@naver.com>
2021-02-01 22:43:15 +0900

    divide conqure

--- Recursion/src/BackJun/P13711.java
+++ Recursion/src/BackJun/P13711.java
@@ -17,8 +17,10 @@ public class P13711 {
 			
 			int[] x = new int[t+1];
 			int[] y = new int[t+1];
+			int[] orderX = new int[t+1];
+			int[] orderY = new int[t+1];
+			
 			int[] lis_x = new int[t+1];
-			int[] lis_y = new int[t+1];
 						
 			line = inbr.readLine();
 			String[] temp = line.split(" ");
@@ -30,61 +32,36 @@ public class P13711 {
 			line = inbr.readLine();
 			temp = line.split(" ");
 
-			/*
 			for(int i=0;i<temp.length;i++) {
 				y[i+1] = Integer.valueOf(temp[i]);
 			}
-			*/
 			
 			for(int i=1;i<x.length;i++) {
-				y[x[i]] = Integer.valueOf(temp[i-1]);
+				orderX[x[i]] = i;
+			}
+
+			for(int i=1;i<y.length;i++) {
+				orderY[orderX[y[i]]] = i;
 			}
 			
+			
+			
 			lis_x[1] = x[1];
-			lis_y[1] = y[1];
-			int i = 1;
-			int j = 1;
-			int k = 1;
-
-			while(i<y.length) {
+			int j=1;
+									
+			for(int i=2;i<x.length;i++) {
 				
-				/*
-				if(lis_x[j] < x[i]) {
-					lis_x[j+1] = x[i];
+				if(lis_x[j] < x[y[i]]) {
 					j++;
+					lis_x[j] = x[y[i]];
 				}
 				else {
-					int idx = binarySearch(lis_x, 1, j, x[i]);
-					lis_x[idx] = x[i];
-				}
-				
-				if(lis_y[k] < y[i]) {
-					lis_y[k+1] = y[i];
-					k++;
-				}
-				else {
-					int idx = binarySearch(lis_y, 1, k, y[i]);
-					lis_y[idx] = x[i];
-				}
-				*/
-				
-				if(lis_y[k] < y[i]) {
-					lis_y[k+1] = y[i];
-					k++;
-				}
-				else {
-					int idx = binarySearch(lis_y, 1, k, y[i]);
-					lis_y[idx] = x[i];
-				}
-
-				i++;
-			}
-						
-			for(int l=1;l<lis_y.length;l++) {
-				if(lis_y[l] != 0) {
-					System.out.print(lis_y[l]+" ");					
+					int idx = binarySearch(lis_x, 1, j, x[y[i]]);
+					lis_x[idx] = x[y[i]];
 				}
 			}
+			
+			System.out.println(j);
 			
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -108,5 +85,55 @@ public class P13711 {
 		}
 		
 		return right;
+	}
+	
+	public static void mergeSort(int[][] value, int left, int right, int col) {
+		
+		int mid = 0;
+		
+		if(left < right) {
+			
+			mid = (left + right)/2;
+			
+			mergeSort(value, left, mid, col);
+			mergeSort(value, mid+1, right, col);
+			merge(value, left, mid, right, col);
+		}
+	}
+	
+	public static void merge(int[][] value, int left, int mid, int right, int col) {
+		
+		int i = left;
+		int j = mid+1;
+		int k = 0;
+		
+		int[][] temp = new int[right - left + 1][2];
+		
+		while(i<=mid && j<= right) {
+			
+			if(value[i][col] < value[j][col]) {
+				temp[k][0] = value[i][0];
+				temp[k++][1] = value[i++][1];
+			}
+			else {
+				temp[k][0] = value[j][0];
+				temp[k++][1] = value[j++][1];
+			}
+		}
+		
+		while(i<=mid) {
+			temp[k][0] = value[i][0];
+			temp[k++][1] = value[i++][1];			
+		}
+		
+		while(j<=right) {
+			temp[k][0] = value[j][0];
+			temp[k++][1] = value[j++][1];			
+		}
+		
+		for(int l=0;l<k;l++) {
+			value[l+left][0] = temp[l][0];
+			value[l+left][1] = temp[l][1];
+		}
 	}
 }
