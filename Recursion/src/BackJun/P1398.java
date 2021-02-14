d2ff8bc18522d7d14240082c1ac27f6ed8710f4a
이정환 <eedys1234@naver.com>
2021-02-14 21:10:54 +0900

    greedy

---
+++ Recursion/src/BackJun/P1398.java
@@ -0,0 +1,31 @@
+package BackJun;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P1398 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader inbr = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			
+			int t = Integer.valueOf(inbr.readLine());
+			
+			while(t-- > 0) {
+				
+				int n = Integer.valueOf(inbr.readLine());
+				
+				//10^k, 25*10^2k
+				while(n > 0) {
+					
+				}
+				
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
