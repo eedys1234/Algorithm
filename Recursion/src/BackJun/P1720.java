b04b9fceab011dbbb768a415b0a95b82cf9ccf1f
이정환 <eedys1234@naver.com>
2021-02-27 23:04:05 +0900

    dp

---
+++ Recursion/src/BackJun/P1720.java
@@ -0,0 +1,20 @@
+package BackJun;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P1720 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader inbr = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			
+			int n = Integer.valueOf(inbr.readLine());
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
