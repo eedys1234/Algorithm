71aaa4d542611adc0b97291c5520060b38c4ea62
이정환 <eedys1234@naver.com>
2021-03-05 21:25:36 +0900

    two pointer

--- Recursion/src/BackJun2/P1644.java
+++ Recursion/src/BackJun2/P1644.java
@@ -1,10 +1,12 @@
 package BackJun2;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class P1644 {
+	
+	public static boolean[] check;
 	
 	public static void main(String[] args) {
 		
@@ -13,14 +15,58 @@ public class P1644 {
 		try {
 			
 			int n = Integer.valueOf(inbr.readLine());
+			List<Integer> primes = new ArrayList<>();
+			check = new boolean[n+1];
+			int res = 0;
+			int start = 0;
+			int end = 0;
+			long sum = 0;
 			
+			for(int i=2;i<=n;i++) {
+				check[i] = true;
+			}
+			
+			//n이하의 소수 구하기
+			setPrime(primes, n);			
+			
+			while(true) {
+				
+				if(sum >= n) {
+					sum -= primes.get(start++);
+				}
+				else if(end == primes.size()) {
+					break;
+				}
+				else {					
+					sum += primes.get(end++);
+				}
+				
+				if(sum == n) res++;
+			}
+			
+			System.out.println(res);
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static boolean isPrime(int k) {
-		return k > 1 &&  IntStream.rangeClosed(2, (int) Math.sqrt(k)).noneMatch(divisor -> k % divisor==0);
+    //에라토스테네스의 체
+    public static void setPrime(List<Integer> primes, int k) {
+    	
+    	for(int i=2;i*i<=k;i++) {
+    		if(!check[i]) continue;
+    		
+    		for(int j=i*i;j<=k;j+=i) 
+    		{
+    			check[j] = false;
+    		}
+    	}
+    	
+		for(int i=2;i<=k;i++) {
+			if(check[i]) {
+				primes.add(i);
+			}
+		}
 	}
 }
