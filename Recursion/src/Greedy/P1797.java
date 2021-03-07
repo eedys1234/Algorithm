fa478e10f43147a82f1b04ded4fa4624cb46e5c8
이정환 <eedys1234@naver.com>
2021-03-07 17:04:09 +0900

    greedy

--- Recursion/src/Greedy/P1797.java
+++ Recursion/src/Greedy/P1797.java
@@ -11,6 +11,7 @@ import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+//greedy
 public class P1797 {
 	
 	public static void main(String[] args) {
@@ -35,22 +36,28 @@ public class P1797 {
 				people.add(new Person(sex, x, i));
 			}
 			
+			//정렬
 			Collections.sort(people, (Person p1, Person p2)-> Integer.compare(p1.getX(), p2.getX()));
 			
+			//n이 2일때
 			if(n==2) {
 				System.out.println(people.get(1).x - people.get(0).x);
 				return;
 			}
 			
+			//성별에 대한 누적합을 통해 어느 위치에서 어느 위치까지 남녀의 수가 동일한지 체크
 			for(int i=0;i<people.size();i++) {
 				sum += people.get(i).sex;
 				people.get(i).setS(sum);
 			}
 			
+			//누적합을 기준으로 그룹핑
 			Map<Integer, List<Person>> group = people.stream().collect(Collectors.groupingBy(Person::getS));
 			
 			int max = 0;
 			
+			//차이의 최대값을 구해야하기 때문에 분류된 그룹에서 x가 가장 큰 값, 작은 값 차이 구함
+			//작은값 다음의 인덱스에서가 핵심
 			for(Entry<Integer, List<Person>> entry : group.entrySet()) {
 				List<Person> p = entry.getValue();				
 				if(p.size() >= 2) {
