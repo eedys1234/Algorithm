7f5548c8037d14fce90edfe8c10eb720446103da
이정환 <eedys1234@naver.com>
2021-03-28 02:39:13 +0900

    lca

---
+++ Recursion/src/Groom_2/E03_1.java
@@ -0,0 +1,8 @@
+package Groom_2;
+
+public class E03_1 {
+
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
---
+++ Recursion/src/LCA/P11437.java
@@ -0,0 +1,109 @@
+package LCA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class P11437 {
+	
+	public static int[] depth;
+	public static int[][] dp;
+	public static int maxLevel; 
+	public static List<List<Integer>> adj;
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader inbr = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		adj = new ArrayList<>();
+		
+		int n = Integer.valueOf(inbr.readLine());
+		depth = new int[100001];
+		dp = new int[100001][20];
+		maxLevel = (int) Math.floor(Math.log(n));
+		
+		for(int i=0;i<n;i++) {
+			adj.add(new ArrayList<>());
+		}
+		
+		for(int i=0;i<n-1;i++) 
+		{
+			String[] temp = inbr.readLine().split(" ");
+			int parent = Integer.valueOf(temp[0]);
+			int child = Integer.valueOf(temp[1]);
+			adj.get(parent-1).add(child);
+			adj.get(child-1).add(parent);
+		}
+		
+		depth[0] = -1;
+		
+		getTree(1, 0);
+		
+		int m = Integer.valueOf(inbr.readLine());
+		
+		for(int i=0;i<m;i++) 
+		{
+			String[] temp = inbr.readLine().split(" ");
+			int a = Integer.valueOf(temp[0]);
+			int b = Integer.valueOf(temp[1]);
+			
+			if(depth[a] != depth[b]) {
+
+				if(depth[a] > depth[b]) {
+					
+					int t = a;
+					a = b;
+					b = t;
+				}
+				
+				for(int j=maxLevel;j>=0;j--) 
+				{
+					if(depth[a] <= depth[dp[b][j]]) {
+						b = dp[b][j];
+					}
+				}
+			}
+			
+			int lca = a;
+			
+			if(a != b) {
+				
+				for(int j=maxLevel;j>=0;j--) 
+				{
+					if(dp[a][j] != dp[b][j]) {
+						a = dp[a][j];
+						b = dp[b][j];						
+					}
+					
+					lca = dp[a][j];
+				}
+			}
+			
+			sb.append(lca).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}	
+	
+	public static void getTree(int here, int parent) {
+		
+		depth[here] = depth[parent] + 1;
+		dp[here][0] = parent;
+		
+		for(int i=1;i<=maxLevel;i++) {			
+			int tmp = dp[here][i-1];
+			dp[here][i] = dp[tmp][i-1];
+		}
+		
+		int len = adj.get(here-1).size();
+		
+		for(int i=0;i<len;i++) 
+		{
+			int next = adj.get(here-1).get(i);
+			if(parent != next) {
+				getTree(next, here);
+			}
+		}
+	}
+}
